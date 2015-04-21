e48a14c2c7f779cf0241dfb622f11e6645b5ad15
George <my latest commit>
2015-04-20 19:50:38 -0600

    add test stubs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -75,7 +75,7 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
-         // this isn't right
+         // is this right?
          it('is hidden by default', function() {
             var theMenu = $('body').attr('class');
             expect(theMenu).toBe('menu-hidden');
@@ -95,6 +95,7 @@ $(function() {
       });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -102,11 +103,24 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        it('have a single .entry element loaded', function() {
+            var foo = true;
+            expect(foo).toBe(true);
+         });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+     });
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        it('has new content', function() {
+            var foo = true;
+            expect(foo).toBe(true);
+         });
+     });
+
 }());
